cbdfff4b6e193795ab861dfa3b1d47cc7d3b6927
Cameron Romo <cromo1225@gmail.com>
2023-12-14 18:23:33 -0700

    Connect products to cart

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -17,7 +17,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create product" do
-    assert_difference("Product.count") do
+    assert_difference "Product.count" do
       post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @title } }
     end
 
@@ -40,8 +40,24 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should destroy product" do
-    assert_difference("Product.count", -1) do
+    assert_difference "Product.count", -1 do
       delete product_url(@product)
+    end
+
+    assert_redirected_to products_url
+  end
+
+  test "cant delte product in cart" do
+    assert_difference "Product.count", 0 do
+      delete product_url products :two
+    end    
+
+    assert_redirected_to products_url
+  end
+  
+  test "should destroy a product" do
+    assert_difference "Product.count", -1 do
+      delete product_url @product
     end
 
     assert_redirected_to products_url
